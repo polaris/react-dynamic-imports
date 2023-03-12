11f719a629461aa448fe474f987ab8a347949abe
Jan Deinhard <jan.deinhard@gmail.com>
2023-03-12 11:22:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+
+const About = React.lazy(() => {
+  return import("./About2");
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div>Hi, This page is Loading...</div>}>
+      <section>
+        <About />
+      </section>
+    </Suspense>
   );
 }
 
